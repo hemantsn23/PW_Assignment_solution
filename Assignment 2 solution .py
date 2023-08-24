b902e1823c8157d7cae030774fb61ef245c25579
hemantsn23 <141107912+hemantsn23@users.noreply.github.com>
2023-08-24 16:32:34 +0530

    Add files via upload

---
+++ Assignment 2 solution .py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#### Q1
+
+
+# In[2]:
+
+
+# comments in python are identified with hash symbol "#"
+# There are three ways to write a comment -
+#as a separate line, beside the corresponding statement of code, or as a multi-line comment block.
+
+
+# In[3]:
+
+
+######################
+
+
+# In[4]:
+
+
+###Q2
+
+
+# In[5]:
+
+
+#A variable is a symbolic name that is a reference or pointer to an object.
+#The assignment operator, denoted by the “=” symbol, is the operator that is used to assign values to variables in Python
+
+
+# In[6]:
+
+
+##############################
+
+
+# In[12]:
+
+
+##Q3
+
+
+# In[13]:
+
+
+#Python defines type conversion functions like int(), float(), str() to directly convert one data type into another
+
+
+# In[17]:
+
+
+#######################################
+
+
+# In[18]:
+
+
+##Q4
+
+
+# In[19]:
+
+
+#To write a Python script, use an ordinary plain text file and add Python instructions
+# to execute pess "shift + enter"
+
+
+# In[20]:
+
+
+######################
+
+
+# In[21]:
+
+
+#Q5
+
+
+# In[25]:
+
+
+my_list = [1, 2, 3, 4, 5]
+
+
+# In[27]:
+
+
+my_list[1:3]
+
+
+# In[28]:
+
+
+###################
+
+
+# In[29]:
+
+
+#Q6
+
+
+# In[30]:
+
+
+# a complex number in mathematics is a number of the form x+yi, x & y are real numbers
+# a complex number in python is a number of the form x+yj, x & y are real numbers
+# in python "j" is used instead of "i",  i is an imaginary number
+
+
+# In[31]:
+
+
+#####################
+
+
+# In[32]:
+
+
+#Q7
+
+
+# In[33]:
+
+
+# correct way to declare a variable named age and assign the value 25 to it is-
+age = 25
+
+
+# In[34]:
+
+
+###################33
+
+
+# In[35]:
+
+
+#Q8
+
+
+# In[36]:
+
+
+price = 9.99
+
+
+# In[37]:
+
+
+type(price)
+
+
+# In[38]:
+
+
+###########################
+
+
+# In[39]:
+
+
+#Q9
+
+
+# In[40]:
+
+
+name=" Hemant Sanjay Nikhade"
+
+
+# In[41]:
+
+
+print(name)      # by using print function
+
+
+# In[42]:
+
+
+###################
+
+
+# In[43]:
+
+
+#Q10
+
+
+# In[44]:
+
+
+str="Hello , World"
+
+
+# In[47]:
+
+
+str[8:13]
+
+
+# In[48]:
+
+
+###################
+
+
+# In[49]:
+
+
+#Q11
+
+
+# In[50]:
+
+
+is_student=True     ### yes i am a student
+
+
+# In[51]:
+
+
+is_student
+
+
+# In[ ]:
+
+
+
+
